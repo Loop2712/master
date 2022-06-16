d492206c50566c9ec5d72901ade3271affdc6cbe
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2022-06-16 13:33:41 +0700

    update theam dashbrod
    -แยกไฟล์ ธีม เป็นไฟล์ย่อยๆ

---
+++ resources/views/layouts/dash/js.blade.php
@@ -0,0 +1,12 @@
+<!-- Vendor JS Files -->
+<script src="{{asset('vendor/NiceAdmin/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{asset('vendor/NiceAdmin/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/NiceAdmin/chart.js/chart.min.js')}}"></script>
+<script src="{{asset('vendor/NiceAdmin/echarts/echarts.min.js')}}"></script>
+<script src="{{asset('vendor/NiceAdmin/quill/quill.min.js')}}"></script>
+<script src="{{asset('vendor/NiceAdmin/simple-datatables/simple-datatables.js')}}"></script>
+<script src="{{asset('vendor/NiceAdmin/tinymce/tinymce.min.js')}}"></script>
+<script src="{{asset('vendor/NiceAdmin/php-email-form/validate.js')}}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{asset('js/NiceAdmin/js/main.js')}}"></script>
