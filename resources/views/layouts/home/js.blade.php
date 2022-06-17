7de5c245d134bef11c0a413d740b978c0e21b491
Loop2712 <52275890+Loop2712@users.noreply.github.com>
2022-06-17 16:36:49 +0700

    update theme
    เพิ่มธีมหน้าแรกสุด wellcome

---
+++ resources/views/layouts/home/js.blade.php
@@ -0,0 +1,11 @@
+<!-- Vendor JS Files -->
+  
+<script src="{{asset('vendor/HeroBiz/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/HeroBiz/aos/aos.js')}}"></script>
+<script src="{{asset('vendor/HeroBiz/glightbox/js/glightbox.min.js')}}"></script>
+<script src="{{asset('vendor/HeroBiz/isotope-layout/isotope.pkgd.min.js')}}"></script>
+<script src="{{asset('vendor/HeroBiz/swiper/swiper-bundle.min.js')}}"></script>
+<script src="{{asset('vendor/HeroBiz/php-email-form/validate.js')}}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{asset('js/HeroBiz/main.js')}}"></script>
